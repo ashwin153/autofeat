7fdde1ca43b80cbb32b1c6c8333a812d175ab7d9
Ashwin Madavan <ashwin.madavan@gmail.com>
2024-09-12 20:38:34 +0000

    feat: add as_of predicates to the filter transform and rename cutoff to window

--- autofeat/transform/filter.py
+++ autofeat/transform/filter.py
@@ -1,4 +1,5 @@
 import dataclasses
+import datetime
 from typing import Any, Iterable
 
 import polars
@@ -11,11 +12,13 @@ from autofeat.transform.base import Transform
 class Filter(Transform):
     """Filter out rows that do not satisfy the predicates.
 
+    :param as_of: Temporal filter.
     :param eq: Equality filters by column.
     :param gt: Greater than filters by column.
     :param lt: Less than filters by column.
     """
 
+    as_of: datetime.datetime | None = None
     eq: dict[str, Any] | None = None
     gt: dict[str, Any] | None = None
     lt: dict[str, Any] | None = None
@@ -26,6 +29,7 @@ class Filter(Transform):
     ) -> Iterable[Table]:
         for table in tables:
             predicates = [
+                *self._as_of_predicates(table),
                 *self._eq_predicates(table),
                 *self._gt_predicates(table),
                 *self._lt_predicates(table),
@@ -36,35 +40,50 @@ class Filter(Transform):
             else:
                 yield table
 
+    def _as_of_predicates(
+        self,
+        table: Table,
+    ) -> Iterable[polars.Expr]:
+        if self.as_of:
+            for column, data_type in table.schema.items():
+                expr = polars.col(column)
+
+                if isinstance(data_type, polars.Datetime):
+                    yield expr.filter(expr < self.as_of)
+                elif isinstance(data_type, polars.Date):
+                    yield expr.filter(expr < self.as_of.date())
+                elif isinstance(data_type, polars.Time):
+                    yield expr.filter(expr <= self.as_of.time())
+
     def _eq_predicates(
         self,
         table: Table,
     ) -> Iterable[polars.Expr]:
         if self.eq:
-            yield from (
-                polars.col(column).filter(polars.col(column).eq(value))
-                for column, value in self.eq.items()
-                if column in table.schema
-            )
+            for column, value in self.eq.items():
+                expr = polars.col(column)
+
+                if column in table.schema:
+                    yield expr.filter(expr.eq(value))
 
     def _gt_predicates(
         self,
         table: Table,
     ) -> Iterable[polars.Expr]:
         if self.gt:
-            yield from (
-                polars.col(column).filter(polars.col(column).gt(value))
-                for column, value in self.gt.items()
-                if column in table.schema
-            )
+            for column, value in self.gt.items():
+                expr = polars.col(column)
+
+                if column in table.schema:
+                    yield expr.filter(expr.gt(value))
 
     def _lt_predicates(
         self,
         table: Table,
     ) -> Iterable[polars.Expr]:
         if self.lt:
-            yield from (
-                polars.col(column).filter(polars.col(column).lt(value))
-                for column, value in self.lt.items()
-                if column in table.schema
-            )
+            for column, value in self.lt.items():
+                expr = polars.col(column)
+
+                if column in table.schema:
+                    yield expr.filter(expr.lt(value))
