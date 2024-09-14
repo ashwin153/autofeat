68757fb2c8d6bc5f6e7b5765e5b96bb45969185c
Ashwin Madavan <ashwin.madavan@gmail.com>
2024-09-14 21:32:43 +0000

    filter out multi row tables

--- autofeat/dataset/base.py
+++ autofeat/dataset/base.py
@@ -32,26 +32,30 @@ class Dataset(abc.ABC):
         filters: list[Filter] | None = None,
         transform: Transform | None = None,
     ) -> polars.LazyFrame:
-        """Extract features under the ``transform`` and for each of the ``filters``.
+        """Extract features under the ``transform`` given each of the ``filters``.
 
         Features are the boolean or numeric columns from tables that reduce to a single row under
-        the ``transform``. A simple way to guarantee this is to always filter and aggregate the data
-        by the same columns.
+        the ``transform``. A simple way to guarantee this is to always filter and aggregate data by
+        the same columns.
 
         :param filters: Filters to apply before transforming the data.
         :param transform: Transform to apply to the data.
         :return: Extracted features.
         """
-        return polars.concat([
-            polars.concat(
-                [
-                    table.data.select(
-                        (polars.selectors.boolean() | polars.selectors.numeric())
-                        .name.suffix(f" from {table.name}"),
-                    )
-                    for table in filter.then(transform or Identity()).apply(self.tables())
-                ],
-                how="horizontal",
-            )
-            for filter in (filters or [Identity()])
-        ])
+        return polars.concat(
+            [
+                polars.concat(
+                    [
+                        table.data
+                        .filter(polars.len() == 1)
+                        .select(
+                            (polars.selectors.boolean() | polars.selectors.numeric())
+                            .name.suffix(f" from {table.name}"),
+                        )
+                        for table in filter.then(transform or Identity()).apply(self.tables())
+                    ],
+                    how="horizontal",
+                )
+                for filter in (filters or [Identity()])
+            ], how="diagonal",
+        )
