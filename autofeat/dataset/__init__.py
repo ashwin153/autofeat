84d95b70c59ca1ed4553e25189fd17f4c79c3cff
Ashwin Madavan <ashwin.madavan@gmail.com>
2024-09-16 03:15:27 +0000

    fix: imports in autofeat/dataset/__init__.py

--- autofeat/dataset/__init__.py
+++ autofeat/dataset/__init__.py
@@ -1,8 +1,13 @@
 __all__ = [
     "Dataset",
+    "DerivedDataset",
     "KaggleCompetition",
     "KaggleDataset",
+    "MergedDataset",
 ]
 
 from autofeat.dataset.base import Dataset
-from autofeat.dataset.kaggle import KaggleCompetition, KaggleDataset
+from autofeat.dataset.derived_dataset import DerivedDataset
+from autofeat.dataset.kaggle_competition import KaggleCompetition
+from autofeat.dataset.kaggle_dataset import KaggleDataset
+from autofeat.dataset.merged_dataset import MergedDataset
