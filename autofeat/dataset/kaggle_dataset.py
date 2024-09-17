a66dbd18204d74380659011724b38c2a789bc4e2
Ashwin Madavan <ashwin.madavan@gmail.com>
2024-09-17 07:57:29 -0700

    feat: support for loading data for a Kaggle competition (#41)

--- autofeat/dataset/kaggle_dataset.py
+++ autofeat/dataset/kaggle_dataset.py
@@ -1,5 +1,7 @@
 import dataclasses
+import os
 import pathlib
+import zipfile
 from collections.abc import Iterable
 
 import polars
@@ -12,9 +14,11 @@ DEFAULT_CACHE = pathlib.Path.home() / ".cache" / "kaggle"
 
 @dataclasses.dataclass(frozen=True, kw_only=True)
 class KaggleDataset(Dataset):
-    """A Kaggle dataset.
+    """A Kaggle dataset or competition.
 
     >>> dataset = KaggleDataset(id="abdullah0a/urban-air-quality-and-health-impact-dataset")
+
+    >>> competition = KaggleDataset(id="house-prices-advanced-regression-techniques")
 
     .. tip::
 
@@ -28,7 +32,7 @@ class KaggleDataset(Dataset):
             chmod 600 ~/.config/kaggle/kaggle.json
 
     :param cache: Path where datasets are locally cached.
-    :param id: Dataset identifier in the form owner/name.
+    :param id: Name of the competition or dataset.
     :param sample_size: Number of rows to sample from each table in the dataset.
     """
 
@@ -42,22 +46,47 @@ class KaggleDataset(Dataset):
         path = self.cache / "datasets" / self.id
         path.mkdir(parents=True, exist_ok=True)
 
-        # TODO: also redownload when the dataset version changes
         if not any(path.iterdir()):
-            # kaggle creates and authenticates a client on import
-            import kaggle
-
-            kaggle.api.dataset_download_files(
-                dataset=self.id,
-                path=str(path),
-                unzip=True,
-            )
+            archive = self._download(path)
+            self._unzip(archive)
 
         for csv in path.glob("*.csv"):
-            df = polars.read_csv(csv)
+            df = polars.read_csv(
+                csv,
+                null_values="NA",
+            )
 
             yield Table(
-                data=polars.scan_csv(csv),
+                data=df.lazy(),
                 name=csv.name,
                 sample=df.sample(min(self.sample_size, len(df))),
             )
+
+    def _download(
+        self,
+        path: pathlib.Path,
+    ) -> pathlib.Path:
+        # kaggle creates and authenticates a client on import
+        import kaggle
+
+        if "/" in self.id:
+            kaggle.api.dataset_download_files(
+                dataset=self.id,
+                path=str(path),
+            )
+        else:
+            kaggle.api.competition_download_files(
+                competition=self.id,
+                path=str(path),
+            )
+
+        return path / f"{self.id}.zip"
+
+    def _unzip(
+        self,
+        archive: pathlib.Path,
+    ) -> None:
+        with zipfile.ZipFile(archive) as zip:
+            zip.extractall(archive.parent)
+
+        os.remove(archive)
