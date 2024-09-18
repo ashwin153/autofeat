83bc45b9dd461ed1ab8a8d446cf893f0f18723e5
Ashwin Madavan <ashwin.madavan@gmail.com>
2024-09-17 23:42:57 -0400

    feat: add support for extracting filters from tables, polars.LazyFrame, and polars.DataFrame (#53)

--- autofeat/dataset/base.py
+++ autofeat/dataset/base.py
@@ -1,17 +1,13 @@
 from __future__ import annotations
 
 import abc
-from collections.abc import Iterable
 from typing import TYPE_CHECKING
 
-import polars
-
-from autofeat.transform.identity import Identity
-
 if TYPE_CHECKING:
+    from collections.abc import Iterable
+
     from autofeat.table import Table
     from autofeat.transform.base import Transform
-    from autofeat.transform.filter import Filter
 
 
 class Dataset(abc.ABC):
@@ -39,46 +35,6 @@ class Dataset(abc.ABC):
         from autofeat.dataset.derived_dataset import DerivedDataset
 
         return DerivedDataset(dataset=self, transform=transform)
-
-    def features(
-        self,
-        *filters: Filter | Iterable[Filter],
-    ) -> polars.LazyFrame:
-        """Extract features for each of the ``filters``.
-
-        Features are the boolean or numeric columns from tables containing a single row. A simple
-        way to guarantee every table has a single row is to aggregate tables by the same columns
-        that they are filtered by.
-
-        :param filters: Filters to apply before transforming the data.
-        :return: Extracted features.
-        """
-        transforms: list[Transform] = [
-            y
-            for x in (filters or (Identity(),))
-            for y in (x if isinstance(x, Iterable) else (x,))
-        ]
-
-        feature_vectors = []
-
-        for transform in transforms:
-            feature_values = []
-
-            for table in transform.apply(self.tables()):
-                feature_selector = (
-                    (polars.selectors.boolean() | polars.selectors.numeric())
-                    .name.suffix(f" from {table.name}")
-                )
-
-                feature_values.append(
-                    table.data
-                    .filter(polars.len() == 1)
-                    .select(feature_selector),
-                )
-
-            feature_vectors.append(polars.concat(feature_values, how="horizontal"))
-
-        return polars.concat(feature_vectors, how="diagonal")
 
     def merge(
         self,
