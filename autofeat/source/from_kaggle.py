523bb7082c84f16382133f6c09c05abf17505fe8
Ashwin Madavan <ashwin.madavan@gmail.com>
2024-09-19 06:08:21 +0000

    fix doctest

--- autofeat/source/from_kaggle.py
+++ autofeat/source/from_kaggle.py
@@ -1,3 +1,4 @@
+import contextlib
 import os
 import pathlib
 import shutil
@@ -23,8 +24,8 @@ def from_kaggle(
 
         Dataset names are prefixed by the name of the owner, but competition names are not.
 
-        >>> dataset = KaggleDataset(name="abdullah0a/urban-air-quality-and-health-impact-dataset")
-        >>> competition = KaggleDataset(name="house-prices-advanced-regression-techniques")
+        >>> dataset = from_kaggle(name="abdullah0a/urban-air-quality-and-health-impact-dataset")
+        >>> competition = from_kaggle(name="house-prices-advanced-regression-techniques")
 
     .. tip::
 
@@ -51,12 +52,13 @@ def from_kaggle(
             # kaggle creates and authenticates a client on import
             import kaggle  # type: ignore[import-untyped]
 
-            if "/" in name:
-                kaggle.api.dataset_download_files(name, str(path))
-                archive = path / f"{kaggle.api.split_dataset_string(name)[1]}.zip"
-            else:
-                kaggle.api.competition_download_files(name, str(path))
-                archive = path / f"{name}.zip"
+            with contextlib.redirect_stdout(None):
+                if "/" in name:
+                    kaggle.api.dataset_download_files(name, str(path))
+                    archive = path / f"{kaggle.api.split_dataset_string(name)[1]}.zip"
+                else:
+                    kaggle.api.competition_download_files(name, str(path))
+                    archive = path / f"{name}.zip"
 
             with zipfile.ZipFile(archive) as zip:
                 zip.extractall(str(archive.parent))
