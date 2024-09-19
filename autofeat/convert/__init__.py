3c6359dd8b132cf66149f988cc06b81a264ba538
Ashwin Madavan <ashwin.madavan@gmail.com>
2024-09-19 19:35:52 +0000

    feat: support for serializing and deserializing datasets

--- autofeat/convert/__init__.py
+++ autofeat/convert/__init__.py
@@ -1,6 +1,12 @@
 __all__ = [
-    "IntoFilters",
     "into_filters",
+    "into_msgpack",
+    "into_tables",
+    "IntoFilters",
+    "IntoMsgpack",
+    "IntoTables",
 ]
 
 from autofeat.convert.into_filters import IntoFilters, into_filters
+from autofeat.convert.into_msgpack import IntoMsgpack, into_msgpack
+from autofeat.convert.into_tables import IntoTables, into_tables
--- autofeat/source/__init__.py
+++ autofeat/source/__init__.py
@@ -1,7 +1,9 @@
 __all__ = [
     "from_kaggle",
+    "from_msgpack",
     "from_tables",
 ]
 
 from autofeat.source.from_kaggle import from_kaggle
+from autofeat.source.from_msgpack import from_msgpack
 from autofeat.source.from_tables import from_tables
