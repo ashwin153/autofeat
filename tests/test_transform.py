8e8a56833fa7782c62db52a814d84cf5b492f68d
Ashwin Madavan <ashwin.madavan@gmail.com>
2024-09-19 10:24:45 -0400

    feat: add parameterized test for Transform.apply and add tests for Cast (#78)
    closes #70

--- tests/test_transform.py
+++ tests/test_transform.py
@@ -1,6 +1,56 @@
+import datetime
+
+import polars.testing
 import pytest
 
+from autofeat.table import Table
 from autofeat.transform import AllOf, AnyOf, Cast, Combine, Encode, Identity, Transform
+
+
+@pytest.mark.parametrize(
+    "transform,given,expected",
+    [
+        (
+            Cast(),
+            [polars.DataFrame({"date": ["2021-02-03"]})],
+            [polars.DataFrame({"date": [datetime.date(2021, 2, 3)]})],
+        ),
+        (
+            Cast(),
+            [polars.DataFrame({"datetime": ["2021-02-03T04:05:06"]})],
+            [polars.DataFrame({"datetime": [datetime.datetime(2021, 2, 3, 4, 5, 6)]})],
+        ),
+        (
+            Cast(),
+            [polars.DataFrame({"time": ["09:30:05"]})],
+            [polars.DataFrame({"time": [datetime.time(9, 30, 5)]})],
+        ),
+        (
+            Cast(),
+            [polars.DataFrame({"cat": ["a", "b", "a"]})],
+            [polars.DataFrame({"cat": ["a", "b", "a"]}, schema={"cat": polars.Categorical()})],
+        ),
+    ],
+)
+def test_apply(
+    transform: Transform,
+    given: list[polars.DataFrame],
+    expected: list[polars.DataFrame],
+) -> None:
+    tables = [
+        Table(data=df.lazy(), name=str(i), sample=df)
+        for i, df in enumerate(given)
+    ]
+
+    actual = [
+        table.data.collect()
+        for table in transform.apply(tables)
+    ]
+
+    assert len(actual) == len(expected)
+
+    for x, y in zip(actual, expected):
+        polars.testing.assert_frame_equal(x, y)
 
 
 @pytest.mark.parametrize(
