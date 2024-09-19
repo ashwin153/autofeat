a6d5cd3629b6dbd83c9ad382a45e2f802da13da5
Ashwin Madavan <ashwin.madavan@gmail.com>
2024-09-19 06:10:16 +0000

    fix init

---
+++ autofeat/__init__.py
@@ -0,0 +1,12 @@
+__all__ = [
+    "Column",
+    "convert",
+    "Dataset",
+    "source",
+    "Table",
+    "transform",
+]
+
+from autofeat import convert, source, transform
+from autofeat.dataset import Dataset
+from autofeat.table import Column, Table
