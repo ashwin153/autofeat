7933c52d6e6cbc8d5c520f57b5f7b1043b1aafd4
Ashwin Madavan <ashwin.madavan@gmail.com>
2024-09-27 12:28:42 -0400

    chore: automatically configure streamlit and ipython in the devcontainer (#102)

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
 set -euo pipefail
 
+# git
+git config --global --add --bool push.autoSetupRemote true
+git config --global --add safe.directory /workspaces/autofeat
+
+# ipython
+mkdir -p ~/.ipython/profile_default/startup
+cat <<EOF >~/.ipython/profile_default/startup/01-autoreload.py
+import IPython
+ipython = IPython.get_ipython()
+ipython.run_line_magic(magic_name="load_ext", line="autoreload")
+ipython.run_line_magic(magic_name="autoreload", line="2")
+EOF
+
+# poetry
 poetry config cache-dir $(pwd)/.cache
 poetry config virtualenvs.in-project true
 poetry completions bash >> ~/.bash_completion
 poetry install
 
-git config --global --add --bool push.autoSetupRemote true
-git config --global --add safe.directory /workspaces/autofeat
-
+# pre-commit
 pre-commit install --install-hooks
+
+# streamlit
+cat >~/.streamlit/config.toml <<EOF
+[browser]
+gatherUsageStats = false
+EOF
