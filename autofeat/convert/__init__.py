203de1a238446dee3fa5879f9baff70d973e0e7f
Ashwin Madavan <ashwin.madavan@gmail.com>
2024-09-28 10:16:28 -0400

    feat: add convert.into_series (#116)
    - This will be used to pass the target vector to the feature selection
    code
    - We only support conversion to series from Polars for now, eventually
    we probably want to add support for Pandas

--- autofeat/convert/__init__.py
+++ autofeat/convert/__init__.py
@@ -4,11 +4,13 @@ __all__ = [
     "into_lazy_frame",
     "into_msgpack",
     "into_schema",
+    "into_series",
     "IntoDataFrame",
     "IntoDataset",
     "IntoLazyFrame",
     "IntoMsgpack",
     "IntoSchema",
+    "IntoSeries",
 ]
 
 from autofeat.convert.into_data_frame import IntoDataFrame, into_data_frame
@@ -16,3 +18,4 @@ from autofeat.convert.into_dataset import IntoDataset, into_dataset
 from autofeat.convert.into_lazy_frame import IntoLazyFrame, into_lazy_frame
 from autofeat.convert.into_msgpack import IntoMsgpack, into_msgpack
 from autofeat.convert.into_schema import IntoSchema, into_schema
+from autofeat.convert.into_series import IntoSeries, into_series
