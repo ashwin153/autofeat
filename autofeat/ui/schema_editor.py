ddf6519b035bcb8215ddd3aa54dc1d60e35bea66
Ashwin Madavan <ashwin.madavan@gmail.com>
2024-09-29 17:51:53 -0400

    fix: caching in the schema_editor component (#140)

--- autofeat/ui/schema_editor.py
+++ autofeat/ui/schema_editor.py
@@ -1,4 +1,4 @@
-from typing import cast
+from typing import Any, cast
 
 import streamlit
 import streamlit.runtime.caching.hashing
@@ -16,35 +16,38 @@ def schema_editor(
     """
     streamlit.title("Edit Schemas")
 
-    edited_tables = []
+    edits = []
 
     for tab, table in zip(
         streamlit.tabs([table.name for table in dataset.tables]),
         dataset.tables,
     ):
         with tab:
-            edited_rows = streamlit.data_editor([
-                {
-                    "column": column,
-                    "redacted": False,
-                    **{
-                        attribute.name: attribute in table.schema[column]
-                        for attribute in Attribute
-                    },
-                }
-                for column in sorted(table.schema)
-            ])
+            rows = _schema_into_rows(table.schema)
+            edited_rows = streamlit.data_editor(rows)
+            edits.append(edited_rows)
 
-            edited_schema = Schema({
-                cast(str, row["column"]): {
-                    attribute
-                    for attribute in Attribute
-                    if row[attribute.name]
-                }
-                for row in edited_rows
-                if not row["redacted"]
-            })
+    return _edit_dataset(dataset, edits)
 
+
+@streamlit.cache_resource(
+    hash_funcs={
+        Dataset: id,
+        list: lambda edits: tuple(
+            tuple(row.values())
+            for edit in cast(list, edits)
+            for row in edit
+        ),
+    },
+)
+def _edit_dataset(
+    dataset: Dataset,
+    edits: list[list[dict[str, Any]]],
+) -> Dataset:
+    edited_tables = []
+
+    for table, edited_rows in zip(dataset.tables, edits):
+        if edited_schema := _schema_from_rows(edited_rows):
             edited_table = Table(
                 data=table.data.select(edited_schema.keys()),
                 name=table.name,
@@ -54,3 +57,33 @@ def schema_editor(
             edited_tables.append(edited_table)
 
     return Dataset(edited_tables)
+
+
+def _schema_into_rows(
+    schema: Schema,
+) -> list[dict[str, Any]]:
+    return [
+        {
+            "column": column,
+            "redacted": False,
+            **{
+                attribute.name: attribute in schema[column]
+                for attribute in Attribute
+            },
+        }
+        for column in sorted(schema)
+    ]
+
+
+def _schema_from_rows(
+    rows: list[dict[str, Any]],
+) -> Schema:
+    return Schema({
+        cast(str, row["column"]): {
+            attribute
+            for attribute in Attribute
+            if row[attribute.name]
+        }
+        for row in rows
+        if not row["redacted"]
+    })
