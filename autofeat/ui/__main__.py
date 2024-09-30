7b9b781505950f5d3ab5d37c86ddc06893cd372c
Ashwin Madavan <ashwin.madavan@gmail.com>
2024-09-30 02:49:40 +0000

    chore: use an expander to hide the schema editor inside the dataset loader

--- autofeat/ui/__main__.py
+++ autofeat/ui/__main__.py
@@ -4,14 +4,12 @@ import streamlit.runtime.caching.hashing
 from autofeat.ui.dataset_explorer import dataset_explorer
 from autofeat.ui.dataset_loader import dataset_loader
 from autofeat.ui.feature_loader import feature_loader
-from autofeat.ui.schema_editor import schema_editor
 
 streamlit.set_page_config(
     page_title="autofeat",
     layout="wide",
 )
 
-if original_dataset := dataset_loader():
-    dataset_explorer(original_dataset)
-    edited_dataset = schema_editor(original_dataset)
-    feature_loader(edited_dataset)
+if dataset := dataset_loader():
+    dataset_explorer(dataset)
+    feature_loader(dataset)
--- autofeat/ui/dataset_loader.py
+++ autofeat/ui/dataset_loader.py
@@ -1,7 +1,9 @@
+from typing import Any, cast
+
 import streamlit
 import streamlit.runtime.caching.hashing
 
-from autofeat import Dataset, source
+from autofeat import Attribute, Dataset, Schema, Table, source
 
 
 def dataset_loader(
@@ -11,7 +13,7 @@ def dataset_loader(
 
     :return: Loaded dataset.
     """
-    streamlit.title("Load Dataset")
+    streamlit.header("Load Dataset")
 
     source_type = streamlit.selectbox(
         "Source Type",
@@ -24,19 +26,40 @@ def dataset_loader(
             accept_multiple_files=True,
         )
 
-        if files:
-            return _load_dataset_from_csv([file.name for file in files])
+        if not files:
+            return None
 
-    if source_type == "Kaggle":
+        dataset = _load_dataset_from_csv([file.name for file in files])
+    elif source_type == "Kaggle":
         name = streamlit.text_input(
-            "Dataset or Competition",
+            "Dataset / Competition",
             placeholder="house-prices-advanced-regression-techniques",
         )
 
-        if name:
-            return _load_dataset_from_kaggle(name)
+        if not name:
+            return None
 
-    return None
+        dataset = _load_dataset_from_kaggle(name)
+    else:
+        raise NotImplementedError(f"{source_type} is not supported")
+
+    with streamlit.expander("Edit Schema"):
+        edits = []
+
+        for tab, table in zip(
+            streamlit.tabs([table.name for table in dataset.tables]),
+            dataset.tables,
+        ):
+            with tab:
+                old = _schema_into_rows(table.schema)
+                new = streamlit.data_editor(old)
+
+                if old == new:
+                    edits.append([])
+                else:
+                    edits.append(new)
+
+        return _edit_dataset(dataset, edits)
 
 
 @streamlit.cache_resource(
@@ -55,3 +78,65 @@ def _load_dataset_from_kaggle(
     name: str,
 ) -> Dataset:
     return source.from_kaggle(name)
+
+
+@streamlit.cache_resource(
+    hash_funcs={
+        Dataset: id,
+        list: lambda edits: tuple(
+            tuple(row.values())
+            for edit in cast(list, edits)
+            for row in edit
+        ),
+    },
+    max_entries=1,
+)
+def _edit_dataset(
+    dataset: Dataset,
+    edits: list[list[dict[str, Any]]],
+) -> Dataset:
+    edited_tables = []
+
+    for table, edited_rows in zip(dataset.tables, edits):
+        if edited_schema := _schema_from_rows(edited_rows):
+            edited_table = Table(
+                data=table.data.select(edited_schema.keys()),
+                name=table.name,
+                schema=edited_schema,
+            )
+
+            edited_tables.append(edited_table)
+        else:
+            edited_tables.append(table)
+
+    return Dataset(edited_tables)
+
+
+def _schema_into_rows(
+    schema: Schema,
+) -> list[dict[str, Any]]:
+    return [
+        {
+            "column": column,
+            "redacted": False,
+            **{
+                attribute.name: attribute in schema[column]
+                for attribute in Attribute
+            },
+        }
+        for column in sorted(schema)
+    ]
+
+
+def _schema_from_rows(
+    rows: list[dict[str, Any]],
+) -> Schema:
+    return Schema({
+        cast(str, row["column"]): {
+            attribute
+            for attribute in Attribute
+            if row[attribute.name]
+        }
+        for row in rows
+        if not row["redacted"]
+    })
