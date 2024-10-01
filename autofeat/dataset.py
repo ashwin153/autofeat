de32a8c030b60d9c3ebb262e94a88d006419ad79
Ashwin Madavan <ashwin.madavan@gmail.com>
2024-10-01 12:58:26 -0400

    feat: refactor feature selection (#153)

--- autofeat/dataset.py
+++ autofeat/dataset.py
@@ -1,11 +1,22 @@
 from __future__ import annotations
 
+import collections
 import dataclasses
 from typing import TYPE_CHECKING
 
 import polars
+import sklearn.model_selection
 
-from autofeat.transform.extract import Extract
+from autofeat.attribute import Attribute
+from autofeat.convert import IntoDataFrame, IntoSeries, into_data_frame, into_series
+from autofeat.model import (
+    PREDICTION_METHODS,
+    SELECTION_METHODS,
+    PredictionMethod,
+    SelectionMethod,
+    TrainedModel,
+)
+from autofeat.transform.keep import Keep
 
 if TYPE_CHECKING:
     from autofeat.convert import IntoDataFrame
@@ -34,9 +45,8 @@ class Dataset:
         """
         return Dataset(list(transform.apply(self.tables)))
 
-    def extract(
+    def features(
         self,
-        *,
         known: IntoDataFrame,
     ) -> polars.DataFrame:
         """Extract features from all tables in this dataset that are relevant to the ``known`` data.
@@ -48,12 +58,89 @@ class Dataset:
         :param known: Data that is already known.
         :return: Extracted features.
         """
+        known = into_data_frame(known)
+
         features = [
-            table.data.select(polars.all().name.suffix(f" from {table.name}"))
-            for table in Extract(known=known).apply(self.tables)
+            (
+                known
+                .lazy()
+                .join(table.data, on=list(primary_key), how="left")
+                .select(polars.selectors.boolean() | polars.selectors.numeric())
+                .select(polars.all().name.suffix(f"::{table.name}"))
+            )
+            for table in self.tables
+            if (primary_key := set(table.schema.select(include={Attribute.primary_key})))
+            if primary_key.issubset(known.columns)
         ]
 
-        return polars.concat(polars.collect_all(features), how="horizontal")
+        return polars.concat(
+            polars.collect_all(features),
+            how="horizontal",
+        )
+
+    def train(
+        self,
+        known: IntoDataFrame,
+        target: IntoSeries,
+        *,
+        prediction_method: PredictionMethod = PREDICTION_METHODS[0],
+        selection_method: SelectionMethod = SELECTION_METHODS[0],
+    ) -> TrainedModel:
+        """Train a model for predicting the ``target`` given the ``known`` information.
+
+        :param known: Data that is already known.
+        :param target: Target variable.
+        :param prediction_method: Method of prediction.
+        :param selection_method: Method of feature selection.
+        :return: Trained model.
+        """
+        # load features from this dataset
+        features = self.features(known)
+
+        # split features and target into training and test data
+        X = features.to_numpy()
+        y = into_series(target).to_numpy()
+        X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y)
+
+        # create a model that predicts the target from the features
+        prediction_model = prediction_method.model()
+
+        # train a model that selects the most important features to the prediction model
+        selection_model = selection_method.model(prediction_model)
+        selection_model.fit(X_train, y_train)
+
+        # apply feature selection to the training and test data
+        X_train = selection_model.transform(X_train)
+        X_test = selection_model.transform(X_test)
+
+        # apply feature selection to this dataset
+        selection = collections.defaultdict(set)
+        for i, selected in enumerate(selection_method.mask(selection_model)):
+            if selected:
+                column, table = features.columns[i].split("::", 1)
+                selection[table].add(column)
+
+        dataset = self.apply(Keep(columns=selection))
+
+        # train the prediction model on the selected features
+        prediction_model.fit(X_train, y_train)
+
+        # evaluate the prediction model on the test data
+        y_pred = prediction_model.predict(X_test)
+
+        # collect all the intermediate outputs
+        return TrainedModel(
+            dataset=dataset,
+            prediction_method=prediction_method,
+            prediction_model=prediction_model,
+            selection_method=selection_method,
+            selection_model=selection_model,
+            X_test=X_test,
+            X_train=X_train,
+            y_pred=y_pred,
+            y_test=y_test,
+            y_train=y_train,
+        )
 
     def table(
         self,
---
+++ autofeat/ui/train_model.py
@@ -0,0 +1,139 @@
+import streamlit
+
+from autofeat.attribute import Attribute
+from autofeat.dataset import Dataset
+from autofeat.model import (
+    PREDICTION_METHODS,
+    SELECTION_METHODS,
+    PredictionMethod,
+    PredictionProblem,
+    SelectionMethod,
+    TrainedModel,
+)
+from autofeat.table import Table
+from autofeat.transform import Aggregate, Cast, Drop, Encode, Identity, Transform
+
+
+def train_model(
+    dataset: Dataset,
+) -> TrainedModel | None:
+    """Load features from the ``dataset`` that are relevant to a prediction problem.
+
+    :param dataset: Dataset to load features from.
+    """
+    streamlit.header("Train Model")
+
+    table = streamlit.selectbox(
+        "Table",
+        dataset.tables,
+        index=None,
+        key="table",
+        on_change=lambda: _clear_state("target_column"),
+    )
+
+    if not table:
+        return None
+
+    target_column = streamlit.selectbox(
+        "Target Column",
+        table.schema,
+        index=None,
+        key="target_column",
+        on_change=lambda: _clear_state("known_columns", "problem"),
+    )
+
+    if not target_column:
+        return None
+
+    known_columns = streamlit.multiselect(
+        "Known Columns",
+        [column for column in table.schema if column != target_column],
+        key="known_columns",
+    )
+
+    if not known_columns:
+        return None
+
+    default_problem = (
+        PredictionProblem.classification
+        if Attribute.categorical in table.schema[target_column]
+        else PredictionProblem.regression
+    )
+
+    problem = streamlit.selectbox(
+        "Problem",
+        list(PredictionProblem),
+        index=default_problem.value - 1,
+        key="problem",
+        on_change=lambda: _clear_state("prediction_method"),
+    )
+
+    prediction_method = streamlit.selectbox(
+        "Prediction Method",
+        [method for method in PREDICTION_METHODS if method.problem == problem],
+        key="prediction_method",
+    )
+
+    selection_method = streamlit.selectbox(
+        "Selection Method",
+        SELECTION_METHODS,
+        key="selection_method",
+    )
+
+    # todo: make this configurable
+    transform = (
+        Drop(columns={table.name: {target_column}})
+        .then(Cast())
+        .then(Encode())
+        .then(Identity(), Aggregate())
+    )
+
+    if not streamlit.button("Train Model"):
+        return None
+
+    return _train_model(
+        dataset=dataset,
+        known_columns=tuple(known_columns),
+        prediction_method=prediction_method,
+        selection_method=selection_method,
+        table=table,
+        target_column=target_column,
+        transform=transform,
+    )
+
+
+@streamlit.cache_resource(
+    hash_funcs={
+        Dataset: id,
+        PredictionMethod: lambda x: x.name,
+        SelectionMethod: lambda x: x.name,
+        Table: id,
+    },
+    max_entries=1,
+)
+def _train_model(
+    *,
+    dataset: Dataset,
+    known_columns: tuple[str, ...],
+    prediction_method: PredictionMethod,
+    selection_method: SelectionMethod,
+    table: Table,
+    target_column: str,
+    transform: Transform,
+) -> TrainedModel:
+    inputs = dataset.apply(transform)
+
+    return inputs.train(
+        known=table.data.select(known_columns),
+        target=table.data.select(target_column),
+        prediction_method=prediction_method,
+        selection_method=selection_method,
+    )
+
+
+def _clear_state(
+    *keys: str,
+) -> None:
+    for key in keys:
+        if key in streamlit.session_state:
+            del streamlit.session_state[key]
