8a50d7a947bf34a5b211ff5ff530b5533c5e61c4
Ashwin Madavan <ashwin.madavan@gmail.com>
2024-10-02 14:21:04 +0000

    cleanup

--- autofeat/ui/evaluate_model.py
+++ autofeat/ui/evaluate_model.py
@@ -5,7 +5,6 @@ import altair
 import numpy
 import pandas
 import shap
-import sklearn._typing
 import sklearn.metrics
 import streamlit
 
@@ -19,17 +18,9 @@ def evaluate_model(
 
     match model.prediction_method.problem:
         case PredictionProblem.classification:
-            metrics = _classification_metrics(
-                model.y_test,
-                model.y_predicted,
-            )
-
-            baseline = _classification_metrics(
-                model.y_test,
-                model.y_baseline,
-            )
-
-            improvement = _pct_chg(baseline["accuracy"], metrics["accuracy"])
+            metrics = _classification_metrics(model.y_test, model.y_predicted)
+            baseline = _classification_metrics(model.y_test, model.y_baseline)
+            improvement = _percent_change(baseline["accuracy"], metrics["accuracy"])
 
             with streamlit.expander(
                 f"✅ model is {improvement:.2f}% more accurate than random guessing",
@@ -37,32 +28,24 @@ def evaluate_model(
                 streamlit.metric(
                     "Accuracy",
                     value=f"{metrics['accuracy']:.4f}",
-                    delta=f"{_pct_chg(baseline['accuracy'], metrics['accuracy'])}%",
+                    delta=f"{_percent_change(baseline['accuracy'], metrics['accuracy']):.2f}%",
                 )
 
                 streamlit.metric(
                     "Precision",
                     value=f"{metrics['precision']:.4f}",
-                    delta=f"{_pct_chg(baseline['precision'], metrics['precision'])}%",
+                    delta=f"{_percent_change(baseline['precision'], metrics['precision']):.2f}%",
                 )
 
                 streamlit.metric(
                     "Recall",
                     value=f"{metrics['recall']:.4f}",
-                    delta=f"{_pct_chg(baseline['recall'], metrics['recall'])}%",
+                    delta=f"{_percent_change(baseline['recall'], metrics['recall']):.2f}%",
                 )
         case PredictionProblem.regression:
-            metrics = _regression_metrics(
-                model.y_test,
-                model.y_predicted,
-            )
-
-            baseline = _regression_metrics(
-                model.y_test,
-                model.y_baseline,
-            )
-
-            improvement = -_pct_chg(baseline["rmse"], metrics["rmse"])
+            metrics = _regression_metrics(model.y_test, model.y_predicted)
+            baseline = _regression_metrics(model.y_test, model.y_baseline)
+            improvement = -_percent_change(baseline["rmse"], metrics["rmse"])
 
             with streamlit.expander(
                 f"✅ model is {improvement:.2f}% more accurate than linear regression",
@@ -70,35 +53,24 @@ def evaluate_model(
                 streamlit.metric(
                     "RMSE",
                     value=f"{metrics['rmse']:.4f}",
-                    delta=f"{_pct_chg(baseline['accuracy'], metrics['accuracy'])}%",
+                    delta=f"{_percent_change(baseline['accuracy'], metrics['accuracy']):.2f}%",
                 )
 
                 streamlit.metric(
                     "R2",
                     value=f"{metrics['r2']:.4f}",
-                    delta=f"{_pct_chg(baseline['r2'], metrics['r2'])}%",
+                    delta=f"{_percent_change(baseline['r2'], metrics['r2']):.2f}%",
                 )
-        case problem:
-            raise NotImplementedError(f"{problem} is not supported")
-
-    features = _explain_features(model)
+        case _:
+            raise NotImplementedError(f"{model.prediction_method.problem} is not supported")
 
     streamlit.altair_chart(
-         (
-            altair.Chart(features)
+        (
+            altair.Chart(_feature_importance(model))
             .mark_bar()
             .configure_axis(labelLimit=300)
-            .encode(
-                y=altair.Y(
-                    "Feature:N",
-                    sort="-x",
-                ),
-                x=altair.X(
-                    "Importance:Q",
-                    scale=altair.Scale(domain=[0, features["Importance"].max()]),
-                ),
-            )
-         ),
+            .encode(y=altair.Y("Feature:N", sort="-x"), x=altair.X("Importance:Q"))
+        ),
         use_container_width=True,
     )
 
@@ -113,8 +85,8 @@ def _classification_metrics(
 ) -> dict[str, Any]:
     return {
         "accuracy": sklearn.metrics.accuracy_score(y_true, y_pred),
-        "precision": sklearn.metrics.precision_score(y_true, y_pred),
-        "recall": sklearn.metrics.recall_score(y_true, y_pred),
+        "precision": sklearn.metrics.precision_score(y_true, y_pred, average="weighted"),
+        "recall": sklearn.metrics.recall_score(y_true, y_pred, average="weighted"),
     }
 
 
@@ -132,21 +104,14 @@ def _regression_metrics(
     }
 
 
-def _pct_chg(
-    old: float,
-    new: float,
-) -> float:
-    return (new - old) / old * 100
-
-
 @streamlit.cache_data(
     hash_funcs={TrainedModel: id},
     max_entries=1,
 )
-def _explain_features(
+def _feature_importance(
     model: TrainedModel,
 ) -> pandas.DataFrame:
-    shap_explainer = shap.Explainer(model.prediction_method)
+    shap_explainer = shap.Explainer(model.prediction_model)
     shap_explanation = shap_explainer(model.X_test)
 
     return pandas.DataFrame(
@@ -155,3 +120,10 @@ def _explain_features(
             "Importance": numpy.abs(shap_explanation.values).mean(0),
         },
     )
+
+
+def _percent_change(
+    old: float,
+    new: float,
+) -> float:
+    return (new - old) / old * 100
