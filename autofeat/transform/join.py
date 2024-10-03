231c0f2f6d3b6a3bb0a14bbae7520ec94ab3ceeb
Ashwin Madavan <ashwin.madavan@gmail.com>
2024-10-03 01:22:05 -0400

    feat: allow join explosion in the join transform (#179)

--- autofeat/transform/join.py
+++ autofeat/transform/join.py
@@ -2,9 +2,6 @@ import dataclasses
 import itertools
 from collections.abc import Iterable
 
-import polars
-import polars._typing
-
 from autofeat.attribute import Attribute
 from autofeat.table import Table
 from autofeat.transform.base import Transform
@@ -12,17 +9,14 @@ from autofeat.transform.base import Transform
 
 @dataclasses.dataclass(frozen=True, kw_only=True)
 class Join(Transform):
-    """Join tables on common primary key columns.
-
-    :param how: Method of joining the tables.
-    """
-
-    how: polars._typing.JoinStrategy = "inner"
+    """Join tables on common primary key columns."""
 
     def apply(
         self,
         tables: Iterable[Table],
     ) -> Iterable[Table]:
+        tables = list(tables)
+
         primary_keys = {
             table.name: set(table.schema.select(include={Attribute.primary_key}))
             for table in tables
@@ -33,15 +27,15 @@ class Join(Transform):
                 x_primary_key = primary_keys[x.name]
                 y_primary_key = primary_keys[y.name]
 
-                if y_primary_key and y_primary_key <= x_primary_key:
+                if y_primary_key and y_primary_key <= x.schema.keys():
                     yield Table(
-                        data=x.data.join(y.data, on=list(y_primary_key), how=self.how),
-                        name=f"{self.how}_join({x.name}, {y.name})",
+                        data=x.data.join(y.data, on=list(y_primary_key), how="left"),
+                        name=f"join({x.name}, {y.name})",
                         schema=y.schema | x.schema,
                     )
-                elif x_primary_key and x_primary_key <= y_primary_key:
+                elif x_primary_key and x_primary_key <= y.schema.keys():
                     yield Table(
-                        data=y.data.join(x.data, on=list(x_primary_key), how=self.how),
-                        name=f"{self.how}_join({y.name}, {x.name})",
+                        data=y.data.join(x.data, on=list(x_primary_key), how="left"),
+                        name=f"join({y.name}, {x.name})",
                         schema=x.schema | y.schema,
                     )
