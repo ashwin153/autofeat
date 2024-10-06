7cea490cffbe271eef7e82e8e0acbec169bb71fe
Ashwin Madavan <ashwin.madavan@gmail.com>
2024-10-06 13:38:38 -0400

    feat: support for Dataset in into_data_frame and into_lazy_frame (#220)

--- autofeat/convert/into_data_frame.py
+++ autofeat/convert/into_data_frame.py
@@ -1,15 +1,18 @@
 from __future__ import annotations
 
-from typing import TypeAlias, Union
+from typing import TYPE_CHECKING, TypeAlias, Union
 
 import polars
 
-from autofeat.table import Table
+if TYPE_CHECKING:
+    from autofeat.dataset import Dataset
+    from autofeat.table import Table
 
 IntoDataFrame: TypeAlias = Union[
     polars.DataFrame,
     polars.LazyFrame,
     "Table",
+    "Dataset",
 ]
 
 
@@ -21,11 +24,19 @@ def into_data_frame(
     :param value: Value to convert.
     :return: Converted data frame.
     """
+    from autofeat.dataset import Dataset
+    from autofeat.table import Table
+
     if isinstance(value, polars.DataFrame):
         return value
     elif isinstance(value, polars.LazyFrame):
         return value.collect()
     elif isinstance(value, Table):
         return value.data.collect()
+    elif isinstance(value, Dataset):
+        return polars.concat(
+            polars.collect_all([table.data for table in value.tables], streaming=True),
+            how="horizontal",
+        )
     else:
         raise NotImplementedError(f"`{type(value)}` cannot be converted to a data frame")
