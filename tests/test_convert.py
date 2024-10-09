b7cb6691963c2ee5030fe7a607f9b0065033e7f3
Ashwin Madavan <ashwin.madavan@gmail.com>
2024-10-09 22:37:13 +0000

    type hints

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -2,6 +2,7 @@ import polars.testing
 import pytest
 
 from autofeat import Column, convert
+from autofeat.convert import IntoNamedExprs, IntoSeries
 
 
 @pytest.mark.parametrize(
@@ -17,7 +18,10 @@ from autofeat import Column, convert
         ),
     ],
 )
-def test_into_named_exprs(given, expected) -> None:
+def test_into_named_exprs(
+    given: IntoNamedExprs,
+    expected: dict[str, polars.Expr],
+) -> None:
     actual = convert.into_named_exprs(given)
     assert len(actual) == len(expected)
     for name, expr in actual.items():
@@ -41,7 +45,10 @@ def test_into_named_exprs(given, expected) -> None:
         ),
     ],
 )
-def test_into_series(given, expected) -> None:
+def test_into_series(
+    given: IntoSeries,
+    expected: polars.Series,
+) -> None:
     polars.testing.assert_series_equal(
         expected,
         convert.into_series(given),
