f41ce1e348a8225b33b227f310fe53c6321a9aca
Ashwin Madavan <ashwin.madavan@gmail.com>
2024-10-09 22:31:54 +0000

    more tests

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -1,19 +1,48 @@
 import polars.testing
 import pytest
 
-from autofeat import convert
+from autofeat import Column, convert
 
 
 @pytest.mark.parametrize(
-    "given",
+    "given,expected",
     [
-        polars.Series("x", [1, 2, 3]),
-        polars.DataFrame({"x": [1, 2, 3]}),
-        polars.LazyFrame({"x": [1, 2, 3]}),
+        (
+            [Column(name="x")],
+            {"x": polars.col("x")},
+        ),
+        (
+            [Column(name="x"), (Column(name="y"), polars.col("x").mean())],
+            {"x": polars.col("x"), "y": polars.col("x").mean()},
+        ),
     ],
 )
-def test_into_series(given) -> None:
+def test_into_named_exprs(given, expected) -> None:
+    actual = convert.into_named_exprs(given)
+    assert len(actual) == len(expected)
+    for name, expr in actual.items():
+        assert str(expr) == str(expected[name])
+
+
+@pytest.mark.parametrize(
+    "given,expected",
+    [
+        (
+            polars.Series("x", [1, 2, 3]),
+            polars.Series("x", [1, 2, 3]),
+        ),
+        (
+            polars.DataFrame({"x": [1, 2, 3]}),
+            polars.Series("x", [1, 2, 3]),
+        ),
+        (
+            polars.LazyFrame({"x": [1, 2, 3]}),
+            polars.Series("x", [1, 2, 3]),
+        ),
+    ],
+)
+def test_into_series(given, expected) -> None:
     polars.testing.assert_series_equal(
-        polars.Series("x", [1, 2, 3]),
+        expected,
         convert.into_series(given),
     )
