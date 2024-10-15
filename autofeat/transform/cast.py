09e4aeb04a3eea0d222faca5f377fd46f42bd840
Ashwin Madavan <ashwin.madavan@gmail.com>
2024-10-15 13:56:38 -0400

    feat: cast columns containing only 0 and 1 to boolean (#306)

--- autofeat/transform/cast.py
+++ autofeat/transform/cast.py
@@ -4,7 +4,6 @@ import attrs
 import polars
 
 from autofeat.attribute import Attribute
-from autofeat.convert.into_exprs import into_exprs
 from autofeat.convert.into_named_exprs import into_named_exprs
 from autofeat.table import Column, Table
 from autofeat.transform.base import Transform
@@ -29,47 +28,21 @@ class Cast(Transform):
         tables = list(tables)
 
         samples = polars.collect_all([
-            (
-                table.data
-                .select(**into_named_exprs(self._castable_columns(table)))
-                .head(SAMPLE_SIZE)
-            )
+            table.data.head(SAMPLE_SIZE)
             for table in tables
         ])
 
         for table, sample in zip(tables, samples):
             casts = [
                 self._cast(table, sample, column)
-                for column in self._castable_columns(table)
-            ]
-
-            extra_columns = [
-                column
                 for column in table.columns
-                if all(column.name != casted_column.name for casted_column, _ in casts)
-            ]
-
-            columns = [
-                *extra_columns,
-                *[column for column, _ in casts],
             ]
 
             yield Table(
-                data=table.data.select(*into_exprs(extra_columns), **into_named_exprs(casts)),
+                data=table.data.select(**into_named_exprs(casts)),
                 name=table.name,
-                columns=columns,
+                columns=[column for column, _ in casts],
             )
-
-    def _castable_columns(
-        self,
-        table: Table,
-    ) -> list[Column]:
-        return [
-            column
-            for column in table.columns
-            if Attribute.textual in column.attributes
-            if Attribute.not_null in column.attributes
-        ]
 
     def _cast(
         self,
@@ -77,24 +50,35 @@ class Cast(Transform):
         sample: polars.DataFrame,
         column: Column,
     ) -> tuple[Column, polars.Expr]:
-        casts = [
-            (column.expr.str.to_date("%Y-%m-%d"), {Attribute.temporal}),
-            (column.expr.str.to_datetime(), {Attribute.temporal}),
-            (column.expr.str.to_time(), {Attribute.temporal}),
-        ]
+        if {Attribute.textual, Attribute.not_null} <= column.attributes:
+            exprs = [
+                column.expr.str.to_date("%Y-%m-%d"),
+                column.expr.str.to_datetime(),
+                column.expr.str.to_time(),
+            ]
 
-        for expr, attributes in casts:
-            try:
-                sample.select(expr)
-            except polars.exceptions.PolarsError:
-                pass
-            else:
-                column = Column(
+            for expr in exprs:
+                try:
+                    sample.select(expr)
+                except polars.exceptions.PolarsError:
+                    pass
+                else:
+                    result = Column(
+                        name=column.name,
+                        attributes={Attribute.temporal},
+                        derived_from=[(column, table)],
+                    )
+
+                    return result, expr
+
+        if {Attribute.numeric, Attribute.not_null} <= column.attributes:
+            if set(sample.get_column(column.name).unique()) == {0, 1}:
+                result = Column(
                     name=column.name,
-                    attributes=attributes,
+                    attributes={Attribute.boolean},
                     derived_from=[(column, table)],
                 )
 
-                return column, expr
+                return result, column.expr.cast(polars.Boolean)
 
         return column, column.expr
