e658e47b34402a17bce042223d9b9dfe8085a77a
Ashwin Madavan <ashwin.madavan@gmail.com>
2024-10-16 14:46:44 -0400

    chore: simplify logic in the union transform and make it more efficient (#319)

--- autofeat/transform/union.py
+++ autofeat/transform/union.py
@@ -1,8 +1,7 @@
-import itertools
+import collections
 from collections.abc import Iterable
 
 import attrs
-import networkx
 import polars
 
 from autofeat.table import Column, Table
@@ -17,9 +16,9 @@ class Union(Transform):
         self,
         tables: Iterable[Table],
     ) -> Iterable[Table]:
-        for related_tables in self._related_tables(list(tables)):
-            if len(related_tables) == 1:
-                yield related_tables[0]
+        for group in self._groups(list(tables)):
+            if len(group) == 1:
+                yield group[0]
             else:
                 columns = [
                     Column(
@@ -27,41 +26,34 @@ class Union(Transform):
                         attributes=set.intersection(
                             *[
                                 c.attributes
-                                for t in related_tables
+                                for t in group
                                 for c in t.columns
                                 if c.name == column.name
                             ],
                         ),
                         derived_from=[
                             (c, t)
-                            for t in related_tables
+                            for t in group
                             for c in t.columns
                             if c.name == column.name
                         ],
                     )
-                    for column in related_tables[0].columns
+                    for column in group[0].columns
                 ]
 
                 yield Table(
-                    name=f"union({', '.join([t.name for t in related_tables])})",
-                    data=polars.concat([t.data for t in related_tables], how="vertical"),
+                    name=f"union({', '.join([t.name for t in group])})",
+                    data=polars.concat([t.data for t in group], how="vertical"),
                     columns=columns,
                 )
 
-    def _related_tables(
+    def _groups(
         self,
         tables: list[Table],
     ) -> list[list[Table]]:
-        graph: networkx.Graph[Table] = networkx.Graph()
-
+        groups = collections.defaultdict(list)
         for table in tables:
-            graph.add_node(table)
+            columns = frozenset(column.name for column in table.columns)
+            groups[columns].append(table)
 
-        for x, y in itertools.combinations(tables, 2):
-            if {c.name for c in x.columns} == {c.name for c in y.columns}:
-                graph.add_edge(x, y)
-
-        return [
-            list(connected_component)
-            for connected_component in networkx.connected_components(graph)
-        ]
+        return list(groups.values())
