c4bd751856d647a124ba31010bc5714468fafb34
Ashwin Madavan <ashwin.madavan@gmail.com>
2024-10-17 01:13:24 +0000

    feat: automatically generate predicates in the filter transform

--- autofeat/transform/filter.py
+++ autofeat/transform/filter.py
@@ -1,9 +1,6 @@
-import datetime
-from collections.abc import Collection, Iterable
-from typing import Any
+from collections.abc import Iterable
 
 import attrs
-import polars
 
 from autofeat.attribute import Attribute
 from autofeat.table import Table
@@ -12,82 +9,29 @@ from autofeat.transform.base import Transform
 
 @attrs.define(frozen=True, kw_only=True, slots=True)
 class Filter(Transform):
-    """Filter out rows that do not satisfy the predicates.
-
-    :param as_of: Temporal constraint.
-    :param eq: Equality constraints.
-    :param gt: Greater-than constraints.
-    :param is_in: Set constraints.
-    :param lt: Less-than constraints.
-    """
-
-    as_of: datetime.datetime | None = None
-    eq: dict[str, Any] | None = None
-    gt: dict[str, Any] | None = None
-    is_in: dict[str, Collection[Any]] | None = None
-    lt: dict[str, Any] | None = None
+    """Filter by categorical columns."""
 
     def apply(
         self,
         tables: Iterable[Table],
     ) -> Iterable[Table]:
         for table in tables:
-            predicates = [
-                *self._as_of_predicates(table),
-                *self._eq_predicates(table),
-                *self._gt_predicates(table),
-                *self._is_in_predicates(table),
-                *self._lt_predicates(table),
+            boolean_columns = [
+                column
+                for column in table.columns
+                if Attribute.boolean in column.attributes
+                if Attribute.not_null in column.attributes
             ]
 
-            if predicates:
-                yield Table(
-                    columns=table.columns,
-                    data=table.data.filter(predicates),
-                    name=table.name,
-                )
+            for x in boolean_columns:
+                predicates = [
+                    (f"{x}", x.expr),
+                    (f"not({x})", x.expr.not_()),
+                ]
 
-    def _as_of_predicates(
-        self,
-        table: Table,
-    ) -> Iterable[polars.Expr]:
-        if self.as_of:
-            for column in table.columns:
-                if Attribute.temporal in column.attributes:
-                    yield column.expr < self.as_of
-
-    def _eq_predicates(
-        self,
-        table: Table,
-    ) -> Iterable[polars.Expr]:
-        if self.eq:
-            for column in table.columns:
-                if value := self.eq.get(column.name):
-                    yield column.expr.eq(value)
-
-    def _gt_predicates(
-        self,
-        table: Table,
-    ) -> Iterable[polars.Expr]:
-        if self.gt:
-            for column in table.columns:
-                if value := self.gt.get(column.name):
-                    yield column.expr.gt(value)
-
-    def _is_in_predicates(
-        self,
-        table: Table,
-    ) -> Iterable[polars.Expr]:
-        if self.is_in:
-            for column in table.columns:
-                if values := self.is_in.get(column.name):
-                    yield column.expr.is_in(values)
-
-    def _lt_predicates(
-        self,
-        table: Table,
-    ) -> Iterable[polars.Expr]:
-        if self.lt:
-            for column in table.columns:
-                if value := self.lt.get(column.name):
-                    yield column.expr.lt(value)
+                for name, expr in predicates:
+                    yield Table(
+                        name=f"filter({table.name}, {name})",
+                        columns=table.columns,
+                        data=table.data.filter(expr),
+                    )
