d2e53ae5b968b608e5c8663434f1143de37e9913
Ashwin Madavan <ashwin.madavan@gmail.com>
2024-10-30 09:04:12 -0700

    feat: add source.from_ndjson (#379)

--- autofeat/source/__init__.py
+++ autofeat/source/__init__.py
@@ -5,6 +5,7 @@ __all__ = [
     "from_excel",
     "from_iceberg",
     "from_kaggle",
+    "from_ndjson",
     "from_parquet",
     "from_sql",
 ]
@@ -15,5 +16,6 @@ from autofeat.source.from_example import from_example
 from autofeat.source.from_excel import from_excel
 from autofeat.source.from_iceberg import from_iceberg
 from autofeat.source.from_kaggle import from_kaggle
+from autofeat.source.from_ndjson import from_ndjson
 from autofeat.source.from_parquet import from_parquet
 from autofeat.source.from_sql import from_sql
