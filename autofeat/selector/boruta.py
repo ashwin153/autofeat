dbdc5b89ef3eb9ec844da926e5311484cfb46d65
Ashwin Madavan <ashwin.madavan@gmail.com>
2024-10-30 20:09:05 +0000

    fix boruta

--- autofeat/selector/boruta.py
+++ autofeat/selector/boruta.py
@@ -1,13 +1,10 @@
 import dataclasses
 from collections.abc import Collection
-from typing import Literal
 
-import BorutaShap
+import boruta
 import numpy
-import pandas
 
 from autofeat.predictor.base import Predictor
-from autofeat.problem import Problem
 from autofeat.selector.base import Selector
 
 
@@ -15,42 +12,24 @@ from autofeat.selector.base import Selector
 class Boruta(Selector):
     """Select the most important features to the ``predictor`` using the Boruta algorithm.
 
-    :param importance: Metric to use to measure feature importance.
     :param predictor: Prediction model.
-    :param problem: Type of prediction problem.
-    :param trials: Number of iterations to run Boruta.
     """
 
-    importance: Literal["shap", "gini", "perm"] = "shap"
     predictor: Predictor
-    problem: Problem
-    trials: int = 20
 
     def select(
         self,
         X: numpy.ndarray,
         y: numpy.ndarray,
     ) -> Collection[bool]:
-        selector = BorutaShap.BorutaShap(
-            classification=self.problem == Problem.classification,
-            importance_measure=self.importance,
-            model=self.predictor,
-            percentile=100,
-            pvalue=0.05,
+        selector = boruta.BorutaPy(
+            estimator=self.predictor,
+            n_estimators="auto",  # pyright: ignore[reportArgumentType]
+            perc=100,
+            alpha=0.05,
+            max_iter=100,
         )
 
-        selector.fit(
-            pandas.DataFrame(X, columns=[str(i) for i in range(X.shape[1])]),
-            y,
-            n_trials=self.trials,
-        )
+        selector.fit(X, y)
 
-        selection = {
-            int(column)
-            for column in selector.accepted + selector.tentative
-        }
-
-        return [
-            i in selection
-            for i in range(X.shape[1])
-        ]
+        return selector.support_  # type: ignore[no-any-return]
