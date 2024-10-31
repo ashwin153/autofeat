43d408ffc0b99728b5f913e1034e55dbeafe75d9
Ashwin Madavan <ashwin.madavan@gmail.com>
2024-10-31 08:07:00 -0700

    feat: automatically detect if a gpu is available using the cuda runtime (#392)

--- autofeat/settings.py
+++ autofeat/settings.py
@@ -1,5 +1,8 @@
 import dataclasses
-from typing import Literal
+import functools
+from typing import Literal, cast
+
+import cuda.cudart
 
 PlotlyTemplate = Literal[
     "ggplot2",
@@ -28,6 +31,15 @@ DisplayMode = Literal[
 ]
 
 
+@functools.cache
+def _cuda_is_available() -> bool:
+    try:
+        cuda_device_count = cast(int, cuda.cudart.cudaGetDeviceCount()[1])
+        return cuda_device_count > 0
+    except Exception:
+        return False
+
+
 @dataclasses.dataclass(kw_only=True)
 class Settings:
     """Global configuration.
@@ -43,7 +55,14 @@ class Settings:
     display_mode: DisplayMode = "minimal"
     low_memory: bool = False
     plotly_template: PlotlyTemplate = "plotly"
-    polars_engine: PolarsEngine = "streaming"
+    polars_engine: PolarsEngine = "gpu" if _cuda_is_available() else "streaming"
+
+    def __post_init__(
+        self,
+    ) -> None:
+        assert (
+            self.polars_engine != "gpu" or _cuda_is_available()
+        ), "cuda must be available to use the gpu"
 
 
 # global configuration
